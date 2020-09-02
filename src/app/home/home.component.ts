5917ca5e0c52a0345783022c5f49ac01f79f3631
koderoom <koderoom.dev@gmail.com>
2020-09-02 15:01:57 +0530

    home.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { faAirFreshener } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  public faAirFreshener = faAirFreshener;
   constructor(private router: Router) {}
 
   ngOnInit(): void {
