ac849c5fada6f83f1246945d9b54981f82ff7403
koderoom <koderoom.dev@gmail.com>
2020-09-02 14:33:32 +0530

    login.register.

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+import { faAirFreshener } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  public faAirFreshener = faAirFreshener;
 
-  constructor() { }
+  public fbFormGroup = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+    email: ['', Validators.required],
+    mobile: ['', Validators.required],
+  });
 
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private http: HttpClient
+  ) {}
+
+  ngOnInit(): void {}
+
+  async registerHere() {
+    const data = this.fbFormGroup.value;
+    const url = 'http://localhost:3000/adduser';
+
+    await this.http.post(url, data).toPromise();
+
+    this.router.navigate(['login']);
   }
-
 }
