17e0d97651bb0cbebc9d11982b63488fba364c16
koderoom <koderoom.dev@gmail.com>
2020-09-02 15:11:50 +0530

    home

--- src/app/personal-info/personal-info.component.ts
+++ src/app/personal-info/personal-info.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-personal-info',
   templateUrl: './personal-info.component.html',
-  styleUrls: ['./personal-info.component.css']
+  styleUrls: ['./personal-info.component.css'],
 })
 export class PersonalInfoComponent implements OnInit {
+  list = [1, 2, 3, 4, 5, 5];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
